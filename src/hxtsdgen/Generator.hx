3a641097dbbf9d2dbc70e3606795f60f164e7e8c
Dan Korostelev <nadako@gmail.com>
2017-02-22 03:08:13 +0300

    support common obsession with asterisks

--- src/hxtsdgen/Generator.hx
+++ src/hxtsdgen/Generator.hx
@@ -89,8 +89,11 @@ class Generator {
         var lines = doc.split("\n");
         for (line in lines) {
             line = line.trim();
-            if (line.length > 0)
-                parts.push('$indent * $line');
+            if (line.length > 0) { // TODO: don't skip empty lines betwen non-empty ones
+                if (line.charCodeAt(0) != "*".code)
+                    line = '* $line';
+                parts.push('$indent $line');
+            }
         }
         parts.push('$indent */');
         return parts.join("\n");
